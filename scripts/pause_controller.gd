2c245deb51a3151d5e9d722d4f6dcdeaccfdfa14
Adriano Félix <adrianofelix019@gmail.com>
2025-12-04 08:28:53 -0300

    fix: pause feature fixed

---
+++ scripts/pause_controller.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+func _ready():
+	process_mode = Node2D.PROCESS_MODE_ALWAYS
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("pause"):
+		print("The paused state is", get_tree().paused)
+		get_tree().paused = !get_tree().paused
